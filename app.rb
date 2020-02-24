74a31cc05e34be471648e20f8f0d543065cbf031
Matthew Jones <matts3371@gmail.com>
2020-02-24 00:24:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   
   post '/piglatinize' do 
     @text = params[:user_phrase].to_s 
-    @piglatinizer = 
+    @piglatinizer = PigLatinizer.new 
     erb:piglatin 
   end 
   
